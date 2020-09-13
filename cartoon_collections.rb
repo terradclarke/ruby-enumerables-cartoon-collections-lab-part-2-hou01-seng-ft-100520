0aa24fc675a18f2734fe5555de76a9e5f3d71f05
Terranie Sims <terranie67@gmail.com>
2020-09-13 23:31:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.each do |planeteer_call|
-   puts "#{planeteer_call.capitalize}!"
+  planeteer_calls.each do |element|
+   puts "#{element.capitalize}!"
   end 
 end
 
@@ -20,4 +20,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
+  planeteer_calls.find? do |element|
+    element == valid_calls
 end

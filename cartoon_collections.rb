2e3773cb770f7341d158172ad0cb6fe6a9fb1011
Terranie Sims <terranie67@gmail.com>
2020-09-14 00:08:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |element|
+  planeteer_calls.select do |element|
     element == valid_calls
   end 
 end
